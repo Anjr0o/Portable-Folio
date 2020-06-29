919ed6364f55884eb81a1839a6294787cf6fe66b
Anjr0o <84.andrew.stokes@gmail.com>
2020-06-29 18:01:39 -0500

    Added ticker search functionality

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_1103aef826214ba59719ee614c8e8e3b")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
 
 def about(request):
     return render(request, 'about.html', {})

87e830456e646f491999a981ab301c7428e95247
Anjr0o <84.andrew.stokes@gmail.com>
2020-07-03 15:18:00 -0500

    Added tables

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -23,3 +26,37 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    import requests
+    import json
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added to the database"))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+
+        output = []
+        for ticker_item in ticker:
+
+            api_request = requests.get(
+                "https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_1103aef826214ba59719ee614c8e8e3b")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+    return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock Deleted"))
+    return redirect(add_stock)

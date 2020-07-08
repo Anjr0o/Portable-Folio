12a034372c974a185b7f2631b54442256695a007
Anjr0o <84.andrew.stokes@gmail.com>
2020-07-07 19:01:30 -0500

    Added Chart.js

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('add_stock.html', views.add_stock, name="add_stock"),
     path('delete/<stock_id>', views.delete, name="delete"),
     path('delete_stock.html', views.delete_stock, name="delete_stock"),
-
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Stock
 from .forms import StockForm
 from django.contrib import messages
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -9,20 +10,28 @@ def home(request):
     import json
 
     if request.method == 'POST':
-        ticker = request.POST['ticker']
+        form = StockForm(request.POST or None)
 
-        api_request = requests.get(
-            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_1103aef826214ba59719ee614c8e8e3b")
-
-        try:
-            api = json.loads(api_request.content)
-        except Exception as e:
-            api = "Error..."
-
-        return render(request, 'home.html', {'api': api})
-
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock Has Been Added to the database"))
+            return redirect('add_stock')
     else:
-        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+        ticker = Stock.objects.all()
+
+        output = []
+        for ticker_item in ticker:
+
+            api_request = requests.get(
+                "https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/chart/5d?token=pk_1103aef826214ba59719ee614c8e8e3b")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+    return render(request, 'home.html', {'ticker': ticker, 'output': output})
 
 def about(request):
     return render(request, 'about.html', {})

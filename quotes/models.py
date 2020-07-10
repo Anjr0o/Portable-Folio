f9529773aca21c57caae0de03d66211bdd92b7c1
Anjr0o <84.andrew.stokes@gmail.com>
2020-07-10 16:59:08 -0500

    Added Quantity

--- quotes/models.py
+++ quotes/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.ticker
+
+class Balance(models.Model):
+    title = models.CharField(default='Balance', max_length=10)
+    amount = models.DecimalField(decimal_places=2, max_digits=8)
+
+    def __str__(self):
+        return self.title
--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Stock
+from .models import Stock, Balance
 from .forms import StockForm
 from django.contrib import messages
 from django.http import JsonResponse
@@ -9,6 +9,8 @@ def home(request):
     import requests
     import json
 
+    default_value = Balance(title='Balance', amount=1500.00, pk=1)
+    default_value.save()
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -55,8 +57,8 @@ def add_stock(request):
             return redirect('add_stock')
     else:
         ticker = Stock.objects.all()
-
         output = []
+        quantities = []
         for ticker_item in ticker:
 
             api_request = requests.get(
@@ -64,18 +66,23 @@ def add_stock(request):
 
             try:
                 api = json.loads(api_request.content)
+                api['quantity'] = Stock.objects.get(ticker=ticker_item).quantity
                 output.append(api)
             except Exception as e:
                 api = "Error..."
 
-    return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+    return render(request, 'add_stock.html', {'ticker': ticker, 'output': output, 'quantities': quantities})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
+    # latestPric = latestPrice * item.
     item.delete()
+
+    Balance.objects.get(pk=1).update(amount=)
     messages.success(request, ("Stock Deleted"))
     return redirect(delete_stock)
 
 def delete_stock(request):
     ticker = Stock.objects.all()
     return render(request, 'delete_stock.html', {'ticker': ticker})
+
